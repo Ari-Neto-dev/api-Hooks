ff18583451fbff79c75d7dc6fdf9e22c94e73199
Ari Azevedo <azevedo.consultor@hotmail.com>
2023-06-30 17:19:47 -0300

    criando botão adicionar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,37 @@
-import React from 'react';
+import React, { useState } from "react";
+
 function App() {
+  const [tarefas, setTarefas] = useState([
+    "Pagar a conta de luz",
+    "Estudar React Hooks",
+  ]);
+
+  const [input, setInput] = useState("");
+
+  function handleAdd() {
+    setTarefas([...tarefas, input]);
+    setInput("");
+  }
+
+  const [nome, setNome] = useState("Ariosvaldo");
+
   return (
     <div>
-      Hooks
+      <ul>
+        {tarefas.map((tarefa) => (
+          <li key={tarefa}>{tarefa}</li>
+        ))}
+      </ul>
+      <h1>{nome}</h1>
+
+      <input
+        type="text"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      />
+      <button type="button" onClick={handleAdd}>
+        Adicionando
+      </button>
     </div>
   );
 }
