70f2ca88cb922a6786141d18ed754c7e605f835d
Ari Azevedo <azevedo.consultor@hotmail.com>
2023-07-05 07:14:51 -0300

    refatorando

--- src/App.js
+++ src/App.js
@@ -1,37 +1,11 @@
-import React, { useState } from "react";
+import React from 'react'
 
 function App() {
-  const [tarefas, setTarefas] = useState([
-    "Pagar a conta de luz",
-    "Estudar React Hooks",
-  ]);
-
-  const [input, setInput] = useState("");
-
-  function handleAdd() {
-    setTarefas([...tarefas, input]);
-    setInput("");
-  }
-
-  const [nome, setNome] = useState("Ariosvaldo");
 
   return (
     <div>
-      <ul>
-        {tarefas.map((tarefa) => (
-          <li key={tarefa}>{tarefa}</li>
-        ))}
-      </ul>
-      <h1>{nome}</h1>
+      oláaaaa tô aqui
 
-      <input
-        type="text"
-        value={input}
-        onChange={(e) => setInput(e.target.value)}
-      />
-      <button type="button" onClick={handleAdd}>
-        Adicionando
-      </button>
     </div>
   );
 }
