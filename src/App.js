0df1f24f1171982deee3a3d55d3c98a5f67c8d96
Ari Azevedo <azevedo.consultor@hotmail.com>
2023-07-05 11:09:29 -0300

    criado um input

--- src/App.js
+++ src/App.js
@@ -1,10 +1,44 @@
-import React from 'react'
+import React,{useState} from 'react'
 
 function App() {
 
+  const[tarefas, setTarefas]=useState([
+    'Pagar a conta de luz',
+    'Estudar React Hooks'
+  ]);
+
+  const[nome,setNome]=useState([
+    ' ARIOSVALDO',
+    ' de Azevedo',
+    ' Neto'
+  ]);
+
+  
+  const[input, setInput]= useState(''); 
+
+  function handleAdd(){
+  
+    setTarefas([...tarefas, input])
+  }
+
+
+
   return (
     <div>
-      oláaaaa tô aqui
+   <ul>
+    {tarefas.map(tarefa=>(
+      <li key={tarefa}>
+        {tarefa}
+      </li>
+
+    ))}
+   </ul>
+
+   <h1>{nome}</h1>
+
+
+<input type="text" value={input} onChange={e => setInput(e.target.value)}/>
+   <button type="button" onClick={handleAdd}>Adicionar</button>
 
     </div>
   );
